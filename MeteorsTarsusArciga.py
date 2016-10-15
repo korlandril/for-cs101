7be963ae5c42f96f55c413a60fd224e2966b7594
korlandril <taa522@mail.umkc.edu>
2016-10-15 15:34:41 -0500

    Starting to figure out how to write grab the coordinates from each line c:

--- MeteorsTarsusArciga.py
+++ MeteorsTarsusArciga.py
@@ -4,7 +4,7 @@
 ##taa522@mail.umkc.edu
 ##
 ##PROBLEM: Take input from a file that contains meteorite data and output specified data from
-## given radii and geolocations from the user.
+## given radii and geolocations from the user to another file.
 ##
 ##ALRGORITHM:
 ##1.  Prompt the user for the file to read from and the file to write to
@@ -16,6 +16,7 @@
 ##ERROR HANDLING:
 ##FileNotFoundError
 ##ValueError
+##
 ##OTHER COMMENTS:
 ##
 ##
@@ -30,13 +31,13 @@ def get_file(prompt, process_type):
     """This gets a file with a set prompt and processing type for the file."""
     while True:
         try:
-            file = open(str(input(prompt)), process_type)
+            file = open(str(input(prompt)), process_type, encoding="utf-8")
             return file
         except FileNotFoundError:
             print("WARNING. Inputted file not found. Please try again.")
 
 def get_coordinates():
-    """This prompts the user for the desired to geolocation to search in."""
+    """This prompts the user for the desired to geolocation to search in and returns a tuple filled with the coordinates."""
 
     while True:
         try:
@@ -68,6 +69,24 @@ def get_radius():
             print("You did not enter a number. Please try again.")
     return radius
 
+def is_within_radius():
+    return False
+    # TODO: STUB
+
+def get_coordinates(document):
+    while True:
+        try:
+            for line in document:
+                coordinates = line[165:].strip()
+                newer = eval(coordinates)
+                print(newer[1])
+            break
+        except NameError:
+            continue
+        except SyntaxError:
+            continue
+    return None
+    # TODO: STUB
 #################################################################################################################
 
 
@@ -75,17 +94,29 @@ def get_radius():
 while exit_ticket == "y" or exit_ticket == "yes":
 
     file_to_read = get_file("Please enter the name of the file you want to pull meteorite data from.", 'r')
-    file_to_write = get_file("Please enter the name of the file you would like to output data to.", 'w')
+    get_coordinates(file_to_read)
+    #TODO: PARSE THE METEORITE COORDINATES WITHOUT AN ERROR PLEASE
+    break
 
-    desired_coordinates = get_coordinates()
-    desired_latitude = desired_coordinates[0]
-    desired_longitude = desired_coordinates[1]
-
-    desired_radius = get_radius()
+    #file_to_write = get_file("Please enter the name of the file you would like to output data to.", 'r+')
 
 
+    # desired_coordinates = get_coordinates()
+    # desired_latitude = desired_coordinates[0]
+    # desired_longitude = desired_coordinates[1]
+    #
+    # desired_radius = get_radius()
 
 
 
+
+
+
+
+
+
+
+# file_to_read.close()
+# file_to_write.close()
 
 
