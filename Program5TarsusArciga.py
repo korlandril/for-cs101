350da2df476df67a7b8cdeb88b5176b245a703ac
korlandril <taa522@mail.umkc.edu>
2016-10-27 12:40:39 -0500

    final for Meteor program

---
+++ Program5TarsusArciga.py
@@ -0,0 +1,21 @@
+##CS101
+##Program 5
+##Tarsus Arciga
+##taa522@mail.umkc.edu
+##
+##PROBLEM: Compare four speeches with four mystery texts and determine the highest word commonality
+##and frequency between the files.
+##
+##ALRGORITHM:
+##yuck.
+##
+##
+##ERROR HANDLING:
+##
+##
+##
+##
+##OTHER COMMENTS:
+##
+##
+
