bef82d92e615df992bae74dc402ad513edcd92a4
korlandril <taa522@mail.umkc.edu>
2016-10-28 14:55:19 -0500

    frequency similarity needed, comparison loop needed, documentation needed.

--- Program5TarsusArciga.py
+++ Program5TarsusArciga.py
@@ -21,9 +21,7 @@
 
 import string
 from collections import Counter
-# def calculate_word_commonality(text1, dict1, text2, dict2):
-#     #TODO: STUB
-#     #Compare two dictionaries????
+
 # def calculate_word_frequency(dict1):
 #     #TODO: STUB
 #     #return a dict
@@ -41,12 +39,23 @@ def file_to_clean_string(file_to_clean, to_exclude):
         if line != "\n":
             new_text += line
 
-    new_text = new_text.lower()
-    new_text = ''.join(ch for ch in new_text if ch not in punctuation)
+    new_text = ''.join(ch for ch in new_text if ch not in punctuation).lower()
 
     clean_text_list = new_text.split()
     no_more_stop = [x for x in clean_text_list if x not in to_exclude]
     return no_more_stop
+
+def calculate_word_commonality(speech1, speech2):
+    first_speech = set(speech1)
+    second_speech = set(speech2)
+
+    first_speech_distinct = len(first_speech)
+    second_speech_distinct = len(second_speech)
+
+    words_in_common = len(first_speech.intersection(second_speech))
+    commonality_percentage = (words_in_common / ((first_speech_distinct + second_speech_distinct) - words_in_common)* 100)
+
+    return commonality_percentage
 
 
 trump = Counter(file_to_clean_string(open("trump.txt", 'r'), stop_list))
@@ -59,19 +68,11 @@ mystery2 = Counter(file_to_clean_string(open("mystery2.txt", 'r'), stop_list))
 mystery3 = Counter(file_to_clean_string(open("mystery3.txt", 'r'), stop_list))
 mystery4 = Counter(file_to_clean_string(open("mystery4.txt", 'r'), stop_list))
 
-trumpset = set(trump)
-mysteryset = set(mystery3)
 
-words_in_common = trumpset.intersection(mysteryset)
 
-print(words_in_common)
-print("Trump's speech has this many distinct words: " + str(len(trump)))
-print("Mystery Speech 3 has this many distinct words: " + str(len(mystery3)))
-print("They have this many words in common: " + str(len(words_in_common)))
 
-commonality_percentage = (len(words_in_common) / ((len(trumpset) + len(mysteryset)) - len(words_in_common))) * 100
-print(commonality_percentage)
-#TODO: Account for word commonality
+print(calculate_word_commonality(romney, mystery2))
+#TODO: Account for frequency similarity
 
 
 
