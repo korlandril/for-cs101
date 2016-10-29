4e32dff4d305b5a6c7f2568e68c9923214c33d07
korlandril <taa522@mail.umkc.edu>
2016-10-29 15:48:53 -0500

    frequency similarity needed, comparison loop needed, documentation needed.

--- Program5TarsusArciga.py
+++ Program5TarsusArciga.py
@@ -20,14 +20,10 @@
 ##
 
 import string
+import copy
+from math import sqrt
 from collections import Counter
 
-# def calculate_word_frequency(dict1):
-#     #TODO: STUB
-#     #return a dict
-# def calculate_relative_frequency(text1, dict1, text2, dict2):
-#     #TODO: STUB
-#     #takes dictionaries. maybe
 stopWords = open("stopWords.txt", 'r').readlines()
 stop_list =[i.strip('\n') for i in stopWords]
 
@@ -53,10 +49,41 @@ def calculate_word_commonality(speech1, speech2):
     second_speech_distinct = len(second_speech)
 
     words_in_common = len(first_speech.intersection(second_speech))
+
     commonality_percentage = (words_in_common / ((first_speech_distinct + second_speech_distinct) - words_in_common)* 100)
 
     return commonality_percentage
 
+def calculate_relative_frequency(speech1):
+    rel_freq = copy.deepcopy(speech1)
+    grand_total = sum(rel_freq.values())
+
+    for key, value in rel_freq.items():
+        rel_freq[key] = rel_freq[key] / grand_total
+
+    return rel_freq
+
+def calculate_frequency_similarity(speech1, speech2):
+    first_speech = set(speech1)
+    second_speech = set(speech2)
+    common_words = first_speech.intersection(second_speech)
+    common_length = len(common_words)
+
+    first_speech_freq = calculate_relative_frequency(speech1)
+    second_speech_freq = calculate_relative_frequency(speech2)
+
+    to_compare = [first_speech_freq, second_speech_freq]
+    common_frequencies = dict()
+
+    for speech in to_compare:
+        for key,value in speech.items():
+            if key in common_words:
+                common_frequencies[key] = (first_speech_freq[key] - second_speech_freq[key]) ** 2
+    sum_of_squares  = sum(common_frequencies.values())
+
+    frequency_similarity = sqrt(sum_of_squares / common_length)
+
+    return frequency_similarity
 
 trump = Counter(file_to_clean_string(open("trump.txt", 'r'), stop_list))
 clinton = Counter(file_to_clean_string(open("clinton.txt", 'r'), stop_list))
@@ -71,7 +98,18 @@ mystery4 = Counter(file_to_clean_string(open("mystery4.txt", 'r'), stop_list))
 
 
 
-print(calculate_word_commonality(romney, mystery2))
+
+#print(calculate_relative_frequency(trump))
+#print(trump)
+
+print(calculate_frequency_similarity(clinton, mystery4))
+
+
+#print(calculate_word_commonality(romney, mystery2))
+#print(calculate_relative_frequency(romney,mystery4))
+
+
+
 #TODO: Account for frequency similarity
 
 
